fd62d930918f4ea598cce7f21ce0c2cdc5cb709f
Filomena Murgo <filomenamurgo@Mac-mini.local>
2023-11-21 17:06:40 +0000

    task 4 completed

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-const {getTopics, getAPI} = require('./db/topics.controller');
+const {getTopics, getAPI} = require('./controllers/topics.controller');
+const { getArticleById } = require('./controllers/articles.controller');
+
+
 
 app.get('/api/topics', getTopics);
 app.get('/api', getAPI);
+app.get('/api/articles/:article_id', getArticleById);
 
 app.use((err, req, res, next)=> {
-    if(err.message === 'Topics not found') {
+    if (err.status === 404) {  
+        res.status(404).send({msg: 'Article not found'})
+      }
         res.status(500).send({ msg: 'Internal Server Error' })
-}
 })
+
 module.exports = app;
